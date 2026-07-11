a6682c7d1c1e5205054a80daf5f23baab973feaf
MatthewAntonioni1221 <mattantonioni745@gmail.com>
2026-07-10 23:59:39 -0400

    remade register javascript file

--- registration/regPage.js
+++ registration/regPage.js
@@ -1,14 +1,14 @@
-document.getElementById('loginForm').addEventListener('submit', async function(e) {e.preventDefault();
+document.getElementById('registerForm').addEventListener('submit', async function(e) {e.preventDefault();
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-
+    const name = document.getElementById('name').value;
     try {
         const response = await fetch('/api/auth/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ username, password })
+            body: JSON.stringify({ username, password, name })
         });
 
         
