94cd22912d0d79717893025586b5e70d09e01889
MatthewAntonioni1221 <mattantonioni745@gmail.com>
2026-08-03 16:41:02 -0400

    changed cors config in hopes of getting preflight to be processed first

--- springbootserver/demo/src/main/java/com/example/demo/CorsConfig.java
+++ springbootserver/demo/src/main/java/com/example/demo/CorsConfig.java
@@ -3,15 +3,18 @@ package com.example.demo;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.Ordered;
 
 @Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("https://rynreading.ca")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);       
     }
